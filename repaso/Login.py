2c93f8c12bf5a37d6a753d4beeae9c62ad199c2f
Dev Jhojan <devjhojan.study@gmail.com>
2025-08-28 20:30:02 -0500

    all

---
+++ repaso/Login.py
@@ -0,0 +1,7 @@
+## Login
+user= input("Write user: ")
+password = input("Write password: ")
+if(user == "admin" and password == "1234"): 
+    print("Wellcome the system")
+else:
+    print("User or Password invalid")
---
+++ repaso/repaso.py
@@ -0,0 +1,29 @@
+# variables
+a = 1  #int
+print(a) # Imprime 1
+a = 1.3 # float
+print(a) # Imprime 1.3
+a = "Hellow" #String
+print(a) # Imprime Hellow
+a = True # boolean
+print(a) # Imprime True
+a = None # Null
+print(a) #imprime None
+
+# Python es un lenguage de bajo tipado por ende no hay que poner tipos a las variables ya que automaticamente cada variable asume el ultimo tipo puesto
+
+# -----------------------------
+# Para trabajar con enteros o decimales hay que hacer un Casting
+age= int(input("pritn your age: "))
+
+# Condicionales
+# if
+if(age > 18):
+    print("Welcome club 'The Sweetheearts'")
+elif(age < 0):
+    print("Age invalid")
+else:
+    print("you can't in to club")
+
+
+    
